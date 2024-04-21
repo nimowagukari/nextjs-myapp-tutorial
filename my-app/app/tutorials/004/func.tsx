3237bbd33d386ff6dbe2c90744f122b0dcc6c427
nimowagukari <nimowagukari@outlook.com>
2024-04-21 01:39:18 +0000

    build エラーを修正2

--- my-app/app/tutorials/004/func.tsx
+++ my-app/app/tutorials/004/func.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 function handleClick(e: PointerEvent) {
-  alert(e.target!.textContent);
+  // https://zenn.dev/usma11dia0/articles/how-to-know-event-type#%E5%9E%8B%E6%8E%A8%E8%AB%96%E3%81%A7%E4%B8%8A%E6%89%8B%E3%81%8F%E3%81%84%E3%81%8B%E3%81%AA%E3%81%84%E5%A0%B4%E5%90%88
+  alert((e.target as Element).textContent);
 }
 
 function MyButton({
