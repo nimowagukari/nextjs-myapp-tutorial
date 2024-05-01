7aa39f9a2dd173015960364e13356e1a2331e3b3
nimowagukari <nimowagukari@outlook.com>
2024-05-01 14:39:06 +0000

    メニューボタンを追加

--- my-app/app/tutorials/010/page.tsx
+++ my-app/app/tutorials/010/page.tsx
@@ -1,3 +1,5 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
 import { Typography } from "@mui/material";
 import { Metadata } from "next";
 
@@ -8,17 +10,24 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <header className="p-8 w-full h-[48px] bg-green-100">header</header>
-      <div className="flex gap-10 h-[500px] bg-blue-100">
-        <div className="max-md:hidden min-w-[300px] w-[300px] bg-red-100">
+      <header className="flex w-full h-[64px] bg-green-200">
+        <div className="w-[64px] md:hidden content-center m-auto">
+          <IconButton size="large">
+            <MenuIcon />
+          </IconButton>
+        </div>
+        <div className="w-full">header</div>
+      </header>
+      <div className="flex h-[500px] bg-gray-200">
+        <div className="max-md:hidden min-w-[300px] w-[300px] p-4 bg-red-200">
           nav
         </div>
-        <div>
+        <div className="w-full p-4 bg-blue-200">
           main
           <Typography variant="h3">Simple Layout 1</Typography>
         </div>
       </div>
-      <footer className="p-8 w-full h-[80px] bg-gray-100">footer</footer>
+      <footer className="p-8 w-full h-[80px] bg-amber-200">footer</footer>
     </>
   );
 }
